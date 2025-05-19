73348e2a3be436e0a0034e0f49fd8248998fc90e
Matias Psara <68296958+matiaspsara@users.noreply.github.com>
2025-05-19 15:25:40 -0300

    fix imgs

--- src/data/gameData.ts
+++ src/data/gameData.ts
@@ -6,55 +6,55 @@ const accessories: Accessory[] = [
     id: 'hat1',
     name: 'Party Hat',
     type: 'hat',
-    src: '/images/hat1.png'
+    src: import.meta.env.BASE_URL + 'images/hat1.png'
   },
   {
     id: 'hat2',
     name: 'Cowboy Hat',
     type: 'hat',
-    src: '/images/hat2.png'
+    src: import.meta.env.BASE_URL + 'images/hat2.png'
   },
   {
     id: 'glasses1',
     name: 'Sunglasses',
     type: 'glasses',
-    src: '/images/glasses1.png'
+    src: import.meta.env.BASE_URL + 'images/glasses1.png'
   },
   {
     id: 'glasses2',
     name: 'Sunglasses',
     type: 'glasses',
-    src: '/images/glasses2.png'
+    src: import.meta.env.BASE_URL + 'images/glasses2.png'
   },
   {
     id: 'shorts1',
     name: 'Short',
     type: 'clothing',
-    src: '/images/shorts1.png'
+    src: import.meta.env.BASE_URL + 'images/shorts1.png'
   },
   {
     id: 'shoeR',
     name: 'shoeR',
     type: 'clothing',
-    src: '/images/shoeR.png'
+    src: import.meta.env.BASE_URL + 'images/shoeR.png'
   },
   {
     id: 'shoeL',
     name: 'shoeL',
     type: 'clothing',
-    src: '/images/shoeL.png'
+    src: import.meta.env.BASE_URL + 'images/shoeL.png'
   },
   {
     id: 'shoe2R',
     name: 'shoe2R',
     type: 'clothing',
-    src: '/images/shoe2R.png'
+    src: import.meta.env.BASE_URL + 'images/shoe2R.png'
   },
   {
     id: 'shoe2L',
     name: 'shoe2L',
     type: 'clothing',
-    src: '/images/shoe2L.png'
+    src: import.meta.env.BASE_URL + 'images/shoe2L.png'
   }
 ];
 
@@ -62,27 +62,27 @@ const accessories: Accessory[] = [
 const cards: Card[] = [
   {
     pairId: 1,
-    image: '/images/florcata1.png'
+    image: import.meta.env.BASE_URL + 'images/florcata1.png'
   },
   {
     pairId: 2,
-    image: '/images/florcata2.png'
+    image: import.meta.env.BASE_URL + 'images/florcata2.png'
   },
   {
     pairId: 3,
-    image: '/images/stitch3.png'
+    image: import.meta.env.BASE_URL + 'images/stitch3.png'
   },
   {
     pairId: 4,
-    image: '/images/stitch4.png'
+    image: import.meta.env.BASE_URL + 'images/stitch4.png'
   },
   {
     pairId: 5,
-    image: '/images/stitch5.png'
+    image: import.meta.env.BASE_URL + 'images/stitch5.png'
   },
   {
     pairId: 6,
-    image: '/images/stitch6.png'
+    image: import.meta.env.BASE_URL + 'images/stitch6.png'
   }
 ];
 
